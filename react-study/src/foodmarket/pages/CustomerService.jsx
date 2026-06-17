2f348a36ea3b2a991dfea94d067231454424a4aa
a77528221-alt <a77528221@gmail.com>
2026-06-17 15:06:24 +0900

    경로별 페이지 처리 설치 및 설정 적용

---
+++ react-study/src/foodmarket/pages/CustomerService.jsx
@@ -0,0 +1,15 @@
+
+
+
+function CustomerService(){
+
+    return (
+        <div>
+            <h1>고객서비스페이지</h1>
+            <h2>정성을 다하겠습니다</h2>
+        </div>
+    )
+
+}
+
+export default CustomerService;
